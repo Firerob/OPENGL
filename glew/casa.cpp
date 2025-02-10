f5dcb7c9fea15b19fe686855c932d7a537c9e5d1
Daniel Felipe Moncada Tello <110641774+Firerob@users.noreply.github.com>
2025-02-09 23:16:06 -0500

    Add files via upload

---
+++ glew/casa.cpp
@@ -0,0 +1,17 @@
+#include "casa.h"
+
+void Casa::crearCasa(float x, float y, float z){
+
+	glPushMatrix();
+
+	glTranslatef(x, y, z);
+
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-1, 0, 0);
+	glVertex3f(1, 0, 0);
+	glVertex3f(0, 1, 0);
+	glEnd();
+
+	glPopMatrix();
+
+}
---
+++ glew/casa.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "glApplication.h"
+#include "glutWindow.h"
+#include <iostream>
+#include "glsl.h"
+#include <time.h>
+
+
+class Casa{
+
+public:
+	Casa() {};
+	void crearCasa(float x, float y, float z);
+};
